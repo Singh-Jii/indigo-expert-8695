2dad7737721c4515114eb743c0a6f9be4d818600
gzbsingh <39863817+gzbsingh@users.noreply.github.com>
2023-02-22 20:39:54 +0530

    Add files via upload

---
+++ scripts/Admin_Home.js
@@ -0,0 +1,54 @@
+
+let baseURL = "https://fashionhub-mrc2.onrender.com";
+let product = 0;
+let users = 0;
+let order = 0;
+
+fetchData("AllProducts")
+fetchData("users")
+fetchData("Orders")
+setTimeout(() => {
+    console.log(product)
+    showGraph()
+}, 2000)
+
+function fetchData(Query) {
+   
+    fetch(`${baseURL}/${Query}`)
+        .then((Response) => {
+            return Response.json()
+        })
+        .then((data) => {
+            console.log(data);
+            Query == "AllProducts" ? product = data.length :Query == "users" ? users = data.length :order = data.length ;
+            
+        })
+
+}
+
+
+
+
+
+
+function showGraph() {
+    google.charts.load('current', { 'packages': ['corechart'] });
+    google.charts.setOnLoadCallback(drawChart);
+
+    function drawChart() {
+        var data = google.visualization.arrayToDataTable([
+            ['Contry', 'Mhl'],
+            ['Products',product ],
+            ['Users',users ],
+            ['Orders',order ]
+        ]);
+
+        var options = {
+            title: 'Company Growth Chart',
+            is3D: true
+        };
+
+        var chart = new google.visualization.PieChart(document.getElementById('myChart'));
+        chart.draw(data, options);
+    }
+}
