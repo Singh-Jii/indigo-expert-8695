90c2a0fce34d75a4eebb4a6036a8550287d3f276
Chandan Kumar Behera <118505620+chandankumar1425@users.noreply.github.com>
2023-02-26 15:42:16 +0530

    chandanfinal

--- signin.js
+++ signin.js
@@ -1,11 +1,16 @@
 var openbtn=document.querySelector("#open-popup-btn");
 var closebtn=document.querySelector(".popup-close-btn");
 let warning=document.getElementById("empty");
+let profile=document.getElementById("profile");
+let accountbtn=document.getElementById("accountbtn");
+let accountsec=document.getElementById("accountsec")
+
 openbtn.addEventListener("click",function(){
     document.body.classList.add("popup-active")
 });
 closebtn.addEventListener("click",function(){
-    document.body.classList.remove("popup-active")
+    document.body.classList.remove("popup-active");
+    location.reload();
 });
 let email=document.getElementById("email")
     let mobile=document.getElementById("mobile")
@@ -42,6 +47,10 @@ headers: {
           alert("Login Sucessfull")
           flag=true;
           warning.innerText=""
+          document.body.classList.remove("popup-active")
+          accountbtn.style.display="block"
+          openbtn.style.display="none"
+
         }
        
        })
@@ -106,3 +115,39 @@ document.getElementById("submit_btn").addEventListener("click", (e) => {
 })
 
 
+
+// submit_btn.addEventListener("click",()=>{
+//   if(fillform.name.value=="" && fillform.mobile.value=="" && fillform.gender.value=="" && 
+// fillform.zipcode.value=="" && fillform.Email.value==""&& fillform.Password.value==""   ){
+//   fill1.innerText="Fill the required feild"
+// }
+// else if(fillform.name.value==""){
+//   fill1.innerText="Fill the Name"
+  
+// }
+// else if(fillform.mobile.value==""){
+//   fill1.innerText="Fill the Mobile Number"
+  
+// }
+// else if(fillform.gender.value==""){
+//   fill1.innerText="Fill the Gender"
+  
+// }
+// else if(fillform.zipcode.value==""){
+//   fill1.innerText="Fill the Zipcode"
+  
+// }
+// else if(fillform.Email.value==""){
+//   fill1.innerText="Fill the Email"
+  
+// }
+// else if(fillform.Password.value==""){
+//   fill1.innerText="Fill the Password"
+  
+// }
+// else{
+//   fill1.innerText=null
+//   fill2.innerText="Registation Sucessfull"
+// }
+
+// })
